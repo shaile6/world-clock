96c93ca6e959a99c7cbb662af1206c3374dbd483
shaile6 <sara.haile@live.com>
2023-04-23 17:04:01 +0100

    Added functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -1,35 +1,64 @@
 function updateTime() {
   // PARIS
   let parisElement = document.querySelector("#paris");
-  // let parisDateElement = document.querySelector("#paris .date");
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTime = moment().tz("Europe/Paris");
-  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
-
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    // let parisDateElement = document.querySelector("#paris .date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    // let parisTimeElement = parisElement.querySelector("#paris .time");
+    let parisTime = moment().tz("Europe/Paris");
+    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
   // JOHANNESBURG
   let johannesburgElement = document.querySelector("#johannesburg");
-  // let johannesburgDateElement = document.querySelector("#johannesburg .date");
-  let johannesburgDateElement = johannesburgElement.querySelector(".date");
-  let johannesburgTimeElement = johannesburgElement.querySelector(".time");
-  let johannesburgTime = moment().tz("Africa/Johannesburg");
-  johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
-  johannesburgTimeElement.innerHTML = johannesburgTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  if (johannesburgElement) {
+    // let johannesburgDateElement = document.querySelector("#johannesburg .date");
+    let johannesburgDateElement = johannesburgElement.querySelector(".date");
+    let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+    let johannesburgTime = moment().tz("Africa/Johannesburg");
+    johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+    johannesburgTimeElement.innerHTML = johannesburgTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // ASMARA
   let asmaraElement = document.querySelector("#asmara");
-  // let asmaraDateElement = document.querySelector("#asmara .date");
-  let asmaraDateElement = asmaraElement.querySelector(".date");
-  let asmaraTimeElement = asmaraElement.querySelector(".time");
-  let asmaraTime = moment().tz("Africa/Asmara");
-  asmaraDateElement.innerHTML = asmaraTime.format("MMMM Do YYYY");
-  asmaraTimeElement.innerHTML = asmaraTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  if (asmaraElement) {
+    // let asmaraDateElement = document.querySelector("#asmara .date");
+    let asmaraDateElement = asmaraElement.querySelector(".date");
+    let asmaraTimeElement = asmaraElement.querySelector(".time");
+    let asmaraTime = moment().tz("Africa/Asmara");
+    asmaraDateElement.innerHTML = asmaraTime.format("MMMM Do YYYY");
+    asmaraTimeElement.innerHTML = asmaraTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )}<small>${cityTime.format("A")}</small></div>
+        </div>
+       `;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
